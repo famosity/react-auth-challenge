84272664029c3c4e67e6732707eff6af700e7a2a
famosity <49885135+famosity@users.noreply.github.com>
2025-06-01 00:52:01 -0300

    feat: add libraries and react-vita

---
+++ react-auth-challenge/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-4xl text-red-500'>Hola mundo </p>
+    </>
+  )
+}
+
+export default App
