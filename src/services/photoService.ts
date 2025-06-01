202fb4f1b1758def7d856592d36d3e873555bc52
famosity <49885135+famosity@users.noreply.github.com>
2025-06-01 18:31:01 -0300

    fix: modify endpoint getPhotos from one to one to twenty content for page

--- src/services/photoService.ts
+++ src/services/photoService.ts
@@ -2,7 +2,9 @@ import axiosInstance from "../api/axiosInstance";
 import type { Photo, ResponseApi } from "../types";
 
 export const getPhotos = async (page:number, limit:number): Promise<ResponseApi> => {
-  const response = await axiosInstance.get<Photo[]>(`photos?_start=${page}&_limit=${limit}`);
+  console.log("page", page)
+ const elements = page === 1 ? 1 : page*20
+  const response = await axiosInstance.get<Photo[]>(`photos?_start=${elements}&_limit=${limit}`);
   return  {
     content : response.data,
     total_elements: 2000,
